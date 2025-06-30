6a45b78a6b3e7e56132e02b85c9494cdf7a0db5d
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-06-30 12:16:54 -0300

    chore: add inicial setup with shadcnui

---
+++ src/components/page-container.tsx
@@ -0,0 +1,43 @@
+export const PageContainer = ({ children }: { children: React.ReactNode }) => {
+  return <div className="w-full space-y-6 p-6">{children}</div>;
+};
+
+export const PageHeader = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex w-full flex-col items-center justify-center gap-4 md:flex-row md:items-center md:justify-between">
+      {children}
+    </div>
+  );
+};
+
+export const PageHeaderContent = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return <div className="flex w-full flex-col space-y-1">{children}</div>;
+};
+
+export const PageTitle = ({ children }: { children: React.ReactNode }) => {
+  return <h1 className="text-2xl font-bold">{children}</h1>;
+};
+
+export const PageDescription = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return <p className="text-muted-foreground text-sm">{children}</p>;
+};
+
+export const PageActions = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex w-full items-center justify-start gap-2 md:justify-end">
+      {children}
+    </div>
+  );
+};
+
+export const PageContent = ({ children }: { children: React.ReactNode }) => {
+  return <div className="space-y-6">{children}</div>;
+};
