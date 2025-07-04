958e2b48bf8064425d123f46f2ddecf786016eb0
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-07-04 15:30:07 -0300

    feat: update CategoryList component to display a message when no categories are available, enhancing user feedback and improving layout consistency

--- src/components/categories/category-list.tsx
+++ src/components/categories/category-list.tsx
@@ -69,69 +69,77 @@ export function CategoryList({
       </div>
 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        {categories.map((category) => (
-          <Card key={category.id}>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <div className="flex items-center space-x-2">
-                <Icon
-                  name={category.icon as IconName}
-                  className="h-9 w-9 rounded-full p-2 text-white"
-                  style={{ backgroundColor: category.color }}
-                />
-                <CardTitle className="text-lg font-semibold">
-                  {category.name}
-                </CardTitle>
-              </div>
-              <div className="flex space-x-2">
-                <CategoryDialog
-                  trigger={
-                    <Button
-                      variant="ghost"
-                      size="sm"
-                      className="p-2 bg-gray-50 hover:bg-gray-100 rounded-full"
-                    >
-                      <Pencil className="h-4 w-4" />
-                    </Button>
-                  }
-                  type={type}
-                  category={category}
-                  onSubmit={(data) => onEditCategory(category.id, data)}
-                />
-                <SubCategoryDialog
-                  trigger={
-                    <Button
-                      variant="ghost"
-                      size="sm"
-                      className="p-2 bg-gray-50 hover:bg-gray-100 rounded-full"
-                    >
-                      <Plus className="h-4 w-4" />
-                    </Button>
-                  }
-                  onSubmit={(name) => onCreateSubcategory(category.id, name)}
-                />
-              </div>
-            </CardHeader>
-            <CardContent>
-              {category.subcategories.length > 0 ? (
-                <div className="space-y-2">
-                  <CardDescription>Subcategorias:</CardDescription>
-                  <div className="flex flex-wrap gap-2">
-                    {category.subcategories.map((subcategory) => (
-                      <div
-                        key={subcategory.id}
-                        className="rounded-full bg-secondary px-3 py-1 text-sm"
-                      >
-                        {subcategory.name}
-                      </div>
-                    ))}
-                  </div>
+        {categories.length > 0 ? (
+          categories.map((category) => (
+            <Card key={category.id}>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <div className="flex items-center space-x-2">
+                  <Icon
+                    name={category.icon as IconName}
+                    className="h-9 w-9 rounded-full p-2 text-white"
+                    style={{ backgroundColor: category.color }}
+                  />
+                  <CardTitle className="text-lg font-semibold">
+                    {category.name}
+                  </CardTitle>
                 </div>
-              ) : (
-                <CardDescription>Nenhuma subcategoria</CardDescription>
-              )}
-            </CardContent>
-          </Card>
-        ))}
+                <div className="flex space-x-2">
+                  <CategoryDialog
+                    trigger={
+                      <Button
+                        variant="ghost"
+                        size="sm"
+                        className="p-2 bg-gray-50 hover:bg-gray-100 rounded-full"
+                      >
+                        <Pencil className="h-4 w-4" />
+                      </Button>
+                    }
+                    type={type}
+                    category={category}
+                    onSubmit={(data) => onEditCategory(category.id, data)}
+                  />
+                  <SubCategoryDialog
+                    trigger={
+                      <Button
+                        variant="ghost"
+                        size="sm"
+                        className="p-2 bg-gray-50 hover:bg-gray-100 rounded-full"
+                      >
+                        <Plus className="h-4 w-4" />
+                      </Button>
+                    }
+                    onSubmit={(name) => onCreateSubcategory(category.id, name)}
+                  />
+                </div>
+              </CardHeader>
+              <CardContent>
+                {category.subcategories.length > 0 ? (
+                  <div className="space-y-2">
+                    <CardDescription>Subcategorias:</CardDescription>
+                    <div className="flex flex-wrap gap-2">
+                      {category.subcategories.map((subcategory) => (
+                        <div
+                          key={subcategory.id}
+                          className="rounded-full bg-secondary px-3 py-1 text-sm"
+                        >
+                          {subcategory.name}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ) : (
+                  <CardDescription>Nenhuma subcategoria</CardDescription>
+                )}
+              </CardContent>
+            </Card>
+          ))
+        ) : (
+          <div className="flex items-center justify-start h-full mt-3">
+            <p className="text-sm text-gray-500">
+              Nenhuma {type === "EXPENSE" ? "despesa" : "receita"} cadastrada
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
